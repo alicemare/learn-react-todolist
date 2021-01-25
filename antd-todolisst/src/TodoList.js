e30a305e76c90bb32962bb389fe0757570bd7622
alicemare <1479070232@qq.com>
2021-01-25 17:57:24 +0800

    reducer init

---
+++ antd-todolisst/src/TodoList.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import 'antd/dist/antd.css';
+import {Input, Button, List} from 'antd';
+import store from './store/index'
+
+
+
+class TodoList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = store.getState();
+        console.log(this.state);
+    }
+    
+    render(){
+        return (
+        
+        <div style={{marginTop:'10px', marginLeft:'10px'}}>
+        <div>
+            <Input value={this.state.inputValue} placeholder='todo info' style={{width:'300px',marginRight:'10px' }} />
+            <Button type="primary">submit</Button>
+        </div>
+        <List
+            style={{marginTop:'10px',width:'300px'}}
+            bordered
+            dataSource={this.state.list}
+            renderItem={item =>(
+                <List.Item>{item}</List.Item>
+            )}
+        
+        />
+
+        </div>)
+    }
+}
+
+export default TodoList;
---
+++ antd-todolisst/src/store/reduer.js
@@ -0,0 +1,13 @@
+const defaultState = {
+    inputValue : '123',
+    list: [
+        'I love u ',
+        'u bad bad',
+        'I like like'
+    ]
+}
+
+export default (state = defaultState, action) => {
+    //state 整个仓库存储数据
+    return state;
+}
