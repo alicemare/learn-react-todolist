70d3b788e5fde4200069e433380aa90217bb921f
alicemare <1479070232@qq.com>
2021-01-25 18:24:31 +0800

    store.subscribe()

--- antd-todolisst/src/TodoList.js
+++ antd-todolisst/src/TodoList.js
@@ -10,7 +10,11 @@ class TodoList extends Component {
     constructor(props){
         super(props);
         this.state = store.getState();
-        console.log(this.state);
+        //console.log(this.state);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStateChange = this.handleStateChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStateChange);
     }
     
     render(){
@@ -18,8 +22,17 @@ class TodoList extends Component {
         
         <div style={{marginTop:'10px', marginLeft:'10px'}}>
         <div>
-            <Input value={this.state.inputValue} placeholder='todo info' style={{width:'300px',marginRight:'10px' }} />
-            <Button type="primary">submit</Button>
+            <Input 
+                value={this.state.inputValue} 
+                placeholder='todo info' 
+                style={{width:'300px',marginRight:'10px' }} 
+                onChange={this.handleInputChange}
+            />
+            <Button 
+                type="primary"
+                onClick={this.handleBtnClick}
+            
+            >submit</Button>
         </div>
         <List
             style={{marginTop:'10px',width:'300px'}}
@@ -32,6 +45,28 @@ class TodoList extends Component {
         />
 
         </div>)
+    }
+
+    handleInputChange(e){
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+        //console.log(e.target.value)
+    }
+
+    handleStateChange(){
+        this.setState(store.getState());
+    }
+
+    handleBtnClick(){
+        const action = {
+            type: 'add_todo_item',
+
+        }
+        store.dispatch(action);
+        this.setState()
     }
 }
 
--- antd-todolisst/src/store/reduer.js
+++ antd-todolisst/src/store/reduer.js
@@ -6,8 +6,23 @@ const defaultState = {
         'I like like'
     ]
 }
-
+//reducer 可以接受state，但是不能修改state
 export default (state = defaultState, action) => {
+    if(action.type === 'change_input_value'){
+        const newState = JSON.parse(JSON.stringify(state));
+        //简易的深拷贝实现
+        newState.inputValue = action.value;
+        return newState;
+    }
+    if(action.type === 'add_todo_item'){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        //console.log(newState)
+        return newState;
+    }
+
+    //console.log(state,action);
     //state 整个仓库存储数据
     return state;
 }
